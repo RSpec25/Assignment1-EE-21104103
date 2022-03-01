54f2b6c8e6e1f49de7eac1e9ffa453e0ab690e48
RSpec25 <97079750+RSpec25@users.noreply.github.com>
2022-03-02 01:33:49 +0530

    Add files via upload

---
+++ Assignment 4-Solution.py
@@ -0,0 +1,71 @@
+/usr/local/bin/python3.9 /Users/rishitsehgal/PycharmProjects/firstprog/Assignment4.py
+********************
+     solution 1     
+********************
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+********************
+     solution 2     
+********************
+Enter the size of pascal triangle 5
+USING ITERATIVE METHOD-WHILE LOOP
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+
+USING RECURSSIONS
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+********************
+     solution 3     
+********************
+Enter 2 numbers with space:-15 3
+PART (a)
+Checking if the quotient and remainder is a callable value or not.
+False
+False
+PART (b)
+Remainder is zero.
+PART (c)
+The filtered numbers that are greater than 4 are : [9, 5, 10, 5, 11, 6]
+PART (d)
+Set: {5, 6, 9, 10, 11}
+PART (e)
+Immutable set:-  frozenset({5, 6, 9, 10, 11})
+PART (f)
+Hash value of the max value from the above set:-  11
+********************
+     solution 4     
+********************
+Object created
+The name of the student is RISHIT SEHGAL and SID is 21104103.
+********************
+     solution 5     
+********************
+part (a)
+(a) The updated salary of Mehak is 70000 
+part (b)
+(b) Record of Viren deleted
+********************
+     solution 6     
+********************
+George's word:-toss
+Barbie's word:-sots
+possible combinations are: ['toss', 'otss', 'osts', 'osst', 'tsos', 'stos', 'sots', 'sost', 'tsso', 'stso', 'ssto', 'ssot', 'toss', 'otss', 'osts', 'osst', 'tsos', 'stos', 'sots', 'sost', 'tsso', 'stso', 'ssto', 'ssot']
+If Barbie's word is present in the list , then their friendship is real. 
+
+The Friendship is real.
+*****ASSIGNMENT 4 END*****
+
+Process finished with exit code 0
